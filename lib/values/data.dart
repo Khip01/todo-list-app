5c66e7343a115aa87cd7f3cd754645649f18a60f
Khip01 <aakhifathallah24@gmail.com>
2024-08-05 01:23:08 +0700

    chore: added some dummy data

---
+++ lib/values/data.dart
@@ -0,0 +1,110 @@
+import '../models/todo.dart';
+
+class Data {
+
+  static List<Todo> dummyTodoList = [
+    TodoListData.todolist_1,
+    TodoListData.todolist_2,
+    TodoListData.todolist_3,
+    TodoListData.todolist_4,
+    TodoListData.todolist_5,
+    TodoListData.todolist_6,
+    TodoListData.todolist_7,
+    TodoListData.todolist_8,
+    TodoListData.todolist_9,
+    TodoListData.todolist_10,
+    TodoListData.todolist_11,
+    TodoListData.todolist_12,
+    TodoListData.todolist_13,
+    TodoListData.todolist_14,
+  ];
+
+}
+
+class TodoListData {
+  static Todo todolist_1 = Todo(
+    id: "tl001",
+    title: "Todo Title 1 Dummy",
+    desc: "Todo Description 1 Dummy ",
+  );
+
+  static Todo todolist_2 = Todo(
+    id: "tl002",
+    title: "Todo Title 2 Dummy",
+    desc: "Todo Description 2 Dummy ",
+  );
+
+  static Todo todolist_3 = Todo(
+    id: "tl003",
+    title: "Todo Title 3 Dummy",
+    desc: "Todo Description 3 Dummy ",
+  );
+
+  static Todo todolist_4 = Todo(
+    id: "tl004",
+    title: "Todo Title 4 Dummy",
+    desc: "Todo Description 4 Dummy ",
+  );
+
+  static Todo todolist_5 = Todo(
+    id: "tl005",
+    title: "Todo Title 5 Dummy",
+    desc: "Todo Description 5 Dummy ",
+  );
+
+  static Todo todolist_6 = Todo(
+    id: "tl006",
+    title: "Todo Title 6 Dummy",
+    desc: "Todo Description 6 Dummy ",
+  );
+
+  static Todo todolist_7 = Todo(
+    id: "tl007",
+    title: "Todo Title 7 Dummy",
+    desc: "Todo Description 7 Dummy ",
+  );
+
+  static Todo todolist_8 = Todo(
+    id: "tl008",
+    title: "Todo Title 8 Dummy",
+    desc: "Todo Description 8 Dummy ",
+  );
+
+  static Todo todolist_9 = Todo(
+    id: "tl009",
+    title: "Todo Title 9 Dummy",
+    desc: "Todo Description 9 Dummy ",
+  );
+
+  static Todo todolist_10 = Todo(
+    id: "tl0010",
+    title: "Todo Title 10 Dummy",
+    desc: "Todo Description 10 Dummy ",
+  );
+
+  static Todo todolist_11 = Todo(
+    id: "tl0011",
+    title: "Todo Title 11 Dummy",
+    desc: "Todo Description 11 Dummy ",
+  );
+
+  static Todo todolist_12 = Todo(
+    id: "tl0012",
+    title: "Todo Title 12 Dummy",
+    desc: "Todo Description 12 Dummy ",
+  );
+
+  static Todo todolist_13 = Todo(
+    id: "tl0013",
+    title: "Todo Title 13 Dummy",
+    desc: "Todo Description 13 Dummy ",
+  );
+
+  static Todo todolist_14 = Todo(
+    id: "tl0014",
+    title: "Todo Title 14 Dummy",
+    desc: "Todo Description 14 Dummy ",
+  );
+
+}
+
