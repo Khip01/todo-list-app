6ecbfb2eeb1e210a2ac479c35687cdd5db03adb0
Khip01 <aakhifathallah24@gmail.com>
2024-08-05 01:28:44 +0700

    feat: activate the callable var

--- lib/utils/style_util.dart
+++ lib/utils/style_util.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class StyleUtil {
 
@@ -10,22 +10,23 @@ class StyleUtil {
   final Color c_97 = const Color.fromARGB(255, 97, 97, 243);
   final Color c_200 = const Color.fromARGB(255, 200, 200, 200);
   final Color c_245 = const Color.fromARGB(255, 245, 245, 245);
+  final Color c_255 = const Color.fromARGB(255, 255, 255, 255);
 
-  // // Font Style
-  //
-  // TextStyle text_Base_Regular =         TextStyle(fontFamily: 'Jost', fontSize: 18,);
-  // TextStyle text_Base_Regular_Italic =  TextStyle(fontFamily: 'Jost', fontSize: 18, fontStyle: FontStyle.italic);
-  // TextStyle text_Base_Medium =          TextStyle(fontFamily: 'Jost', fontSize: 18, fontWeight: FontWeight.w500);
-  // TextStyle text_Base_Medium_Italic =   TextStyle(fontFamily: 'Jost', fontSize: 18, fontWeight: FontWeight.w500, fontStyle: FontStyle.italic);
-  // TextStyle text_Base_Bold =            TextStyle(fontFamily: 'Jost', fontSize: 18, fontWeight: FontWeight.w700);
-  // TextStyle text_Base_Bold_Italic =     TextStyle(fontFamily: 'Jost', fontSize: 18, fontWeight: FontWeight.w700, fontStyle: FontStyle.italic);
-  //
-  // TextStyle text_xl_Regular =           TextStyle(fontFamily: 'Jost', fontSize: 24,);
-  // TextStyle text_xl_Regular_Italic =    TextStyle(fontFamily: 'Jost', fontSize: 24, fontStyle: FontStyle.italic);
-  // TextStyle text_xl_Medium =            TextStyle(fontFamily: 'Jost', fontSize: 24, fontWeight: FontWeight.w500);
-  // TextStyle text_xl_Medium_Italic =     TextStyle(fontFamily: 'Jost', fontSize: 24, fontWeight: FontWeight.w500, fontStyle: FontStyle.italic);
-  // TextStyle text_xl_Bold =              TextStyle(fontFamily: 'Jost', fontSize: 24, fontWeight: FontWeight.w700);
-  // TextStyle text_xl_Bold_Italic =       TextStyle(fontFamily: 'Jost', fontSize: 24, fontWeight: FontWeight.w700, fontStyle: FontStyle.italic);
+  // Font Style
+
+  TextStyle text_Base_Regular =         TextStyle(fontFamily: 'Jost', fontSize: 14,);
+  TextStyle text_Base_Regular_Italic =  TextStyle(fontFamily: 'Jost', fontSize: 14, fontStyle: FontStyle.italic);
+  TextStyle text_Base_Medium =          TextStyle(fontFamily: 'Jost', fontSize: 14, fontWeight: FontWeight.w500);
+  TextStyle text_Base_Medium_Italic =   TextStyle(fontFamily: 'Jost', fontSize: 14, fontWeight: FontWeight.w500, fontStyle: FontStyle.italic);
+  TextStyle text_Base_Bold =            TextStyle(fontFamily: 'Jost', fontSize: 14, fontWeight: FontWeight.w700);
+  TextStyle text_Base_Bold_Italic =     TextStyle(fontFamily: 'Jost', fontSize: 14, fontWeight: FontWeight.w700, fontStyle: FontStyle.italic);
+
+  TextStyle text_xl_Regular =           TextStyle(fontFamily: 'Jost', fontSize: 18,);
+  TextStyle text_xl_Regular_Italic =    TextStyle(fontFamily: 'Jost', fontSize: 18, fontStyle: FontStyle.italic);
+  TextStyle text_xl_Medium =            TextStyle(fontFamily: 'Jost', fontSize: 18, fontWeight: FontWeight.w500);
+  TextStyle text_xl_Medium_Italic =     TextStyle(fontFamily: 'Jost', fontSize: 18, fontWeight: FontWeight.w500, fontStyle: FontStyle.italic);
+  TextStyle text_xl_Bold =              TextStyle(fontFamily: 'Jost', fontSize: 18, fontWeight: FontWeight.w700);
+  TextStyle text_xl_Bold_Italic =       TextStyle(fontFamily: 'Jost', fontSize: 18, fontWeight: FontWeight.w700, fontStyle: FontStyle.italic);
 
 }
 
