bfe81b0d17f19597dd8141c5a8d18ac3965e19b8
Khip01 <aakhifathallah24@gmail.com>
2024-08-06 23:43:29 +0700

    feat: add some dummy data
    dummy data for init todo list

--- lib/values/data.dart
+++ lib/values/data.dart
@@ -26,84 +26,98 @@ class TodoListData {
     id: "tl001",
     title: "Todo Title 1 Dummy",
     desc: "Todo Description 1 Dummy ",
+    check: false,
   );
 
   static Todo todolist_2 = Todo(
     id: "tl002",
     title: "Todo Title 2 Dummy",
     desc: "Todo Description 2 Dummy ",
+    check: false,
   );
 
   static Todo todolist_3 = Todo(
     id: "tl003",
     title: "Todo Title 3 Dummy",
     desc: "Todo Description 3 Dummy ",
+    check: false,
   );
 
   static Todo todolist_4 = Todo(
     id: "tl004",
     title: "Todo Title 4 Dummy",
     desc: "Todo Description 4 Dummy ",
+    check: false,
   );
 
   static Todo todolist_5 = Todo(
     id: "tl005",
     title: "Todo Title 5 Dummy",
     desc: "Todo Description 5 Dummy ",
+    check: false,
   );
 
   static Todo todolist_6 = Todo(
     id: "tl006",
     title: "Todo Title 6 Dummy",
     desc: "Todo Description 6 Dummy ",
+    check: false,
   );
 
   static Todo todolist_7 = Todo(
     id: "tl007",
     title: "Todo Title 7 Dummy",
     desc: "Todo Description 7 Dummy ",
+    check: false,
   );
 
   static Todo todolist_8 = Todo(
     id: "tl008",
     title: "Todo Title 8 Dummy",
     desc: "Todo Description 8 Dummy ",
+    check: false,
   );
 
   static Todo todolist_9 = Todo(
     id: "tl009",
     title: "Todo Title 9 Dummy",
     desc: "Todo Description 9 Dummy ",
+    check: false,
   );
 
   static Todo todolist_10 = Todo(
     id: "tl0010",
     title: "Todo Title 10 Dummy",
     desc: "Todo Description 10 Dummy ",
+    check: false,
   );
 
   static Todo todolist_11 = Todo(
     id: "tl0011",
     title: "Todo Title 11 Dummy",
     desc: "Todo Description 11 Dummy ",
+    check: false,
   );
 
   static Todo todolist_12 = Todo(
     id: "tl0012",
     title: "Todo Title 12 Dummy",
     desc: "Todo Description 12 Dummy ",
+    check: false,
   );
 
   static Todo todolist_13 = Todo(
     id: "tl0013",
     title: "Todo Title 13 Dummy",
     desc: "Todo Description 13 Dummy ",
+    check: false,
   );
 
   static Todo todolist_14 = Todo(
     id: "tl0014",
     title: "Todo Title 14 Dummy",
     desc: "Todo Description 14 Dummy ",
+    check: false,
   );
 
 }
