6cc2cd351fb3a116a3142e128bbfc375a7b8db2e
Khip01 <aakhifathallah24@gmail.com>
2024-08-06 23:45:24 +0700

    feat: added id generator helper

---
+++ lib/utils/helper/generate_todo_index.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_list_app/screens/home/blocs/todo_list/todo_list_bloc.dart';
+
+int generateTodoIndex(BuildContext context){
+  final TodoListState todoListState = context.read<TodoListBloc>().state;
+  if (todoListState.todoList.isEmpty) return 0;
+  return todoListState.todoList.length;
+}
