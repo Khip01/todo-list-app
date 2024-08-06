80ad591035bf74bab07cb5bbd0a91177bcb56823
Khip01 <aakhifathallah24@gmail.com>
2024-08-06 23:47:06 +0700

    feat: added some required todo model

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,12 +1,37 @@
-
 class Todo {
   final String id;
   final String title;
   final String desc;
+  final bool check;
 
   Todo({
     required this.id,
     required this.title,
     required this.desc,
+    required this.check,
+  });
+
+  Todo copyWith({
+    String? id,
+    String? title,
+    String? desc,
+    bool? check,
+  }) {
+    return Todo(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      desc: desc ?? this.desc,
+      check: check ?? this.check,
+    );
+  }
+}
+
+class TodoRequirement {
+  final bool titleIsError;
+  final bool descIsError;
+
+  TodoRequirement({
+    required this.titleIsError,
+    required this.descIsError,
   });
 }
