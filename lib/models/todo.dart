c67227d598e33042e51b85b4adc1faaad3752026
Khip01 <aakhifathallah24@gmail.com>
2024-08-15 00:34:27 +0700

    feat: update the todo model and customize it  to fit in the database
    also added some String const for field/table naming

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,3 +1,18 @@
+const String todoTableName = "todo";
+
+// initialise the todo table in the sqflite database
+class TodoTable {
+  static const String id = "id";
+  static const String title = "title";
+  static const String desc = "description";
+  static const String check = "isChecked";
+
+  static const String idType = "TEXT PRIMARY KEY";
+  static const String titleType = "TEXT NOT NULL";
+  static const String descType = "TEXT NOT NULL";
+  static const String checkType = "INTEGER NOT NULL";
+}
+
 class Todo {
   final String id;
   final String title;
@@ -24,6 +39,20 @@ class Todo {
       check: check ?? this.check,
     );
   }
+
+  static Todo fromJson(Map<String, dynamic> json) => Todo(
+        id: json[TodoTable.id] as String,
+        title: json[TodoTable.title] as String,
+        desc: json[TodoTable.desc] as String,
+        check: json[TodoTable.check] == 1,
+      );
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+    TodoTable.id : id,
+    TodoTable.title : title,
+    TodoTable.desc : desc,
+    TodoTable.check : check ? 1 : 0,
+  };
 }
 
 class TodoRequirement {
