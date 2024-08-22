f830873f1c468858b4ba0fbfebd59e7e18d4b785
Khip01 <aakhifathallah24@gmail.com>
2024-08-23 00:01:55 +0700

    feat(init): init local notif feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_app/utils/helper/local_notification_helper.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  // Init Notification And Local Timezone
+  WidgetsFlutterBinding.ensureInitialized();
+  await LocalNotificationHelper.initLocalNotification();
+
   runApp(MainApp());
 }
