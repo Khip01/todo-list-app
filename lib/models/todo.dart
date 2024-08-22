e80252d3e3dc978d5a0f81a54eda44aec6db67b2
Khip01 <aakhifathallah24@gmail.com>
2024-08-23 00:00:29 +0700

    feat: models changes due to db

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -6,11 +6,13 @@ class TodoTable {
   static const String title = "title";
   static const String desc = "description";
   static const String check = "isChecked";
+  static const String scheduledTime = "scheduledTime";
 
   static const String idType = "TEXT PRIMARY KEY";
   static const String titleType = "TEXT NOT NULL";
   static const String descType = "TEXT NOT NULL";
   static const String checkType = "INTEGER NOT NULL";
+  static const String scheduledTimeType = "TEXT";
 }
 
 class Todo {
@@ -18,12 +20,14 @@ class Todo {
   final String title;
   final String desc;
   final bool check;
+  final String? scheduledTime; // optional
 
   Todo({
     required this.id,
     required this.title,
     required this.desc,
     required this.check,
+    this.scheduledTime,
   });
 
   Todo copyWith({
@@ -31,12 +35,14 @@ class Todo {
     String? title,
     String? desc,
     bool? check,
+    String? scheduledTime,
   }) {
     return Todo(
       id: id ?? this.id,
       title: title ?? this.title,
       desc: desc ?? this.desc,
       check: check ?? this.check,
+      scheduledTime: scheduledTime ?? this.scheduledTime,
     );
   }
 
@@ -45,14 +51,16 @@ class Todo {
         title: json[TodoTable.title] as String,
         desc: json[TodoTable.desc] as String,
         check: json[TodoTable.check] == 1,
+        scheduledTime: json[TodoTable.scheduledTime] as String?,
       );
 
   Map<String, dynamic> toJson() => <String, dynamic>{
-    TodoTable.id : id,
-    TodoTable.title : title,
-    TodoTable.desc : desc,
-    TodoTable.check : check ? 1 : 0,
-  };
+        TodoTable.id: id,
+        TodoTable.title: title,
+        TodoTable.desc: desc,
+        TodoTable.check: check ? 1 : 0,
+        TodoTable.scheduledTime: scheduledTime,
+      };
 }
 
 class TodoRequirement {
